513c8fba99ac35256dbc9cf558f6575a5203abcf
PoshSquash <57773399+PoshSquash@users.noreply.github.com>
2020-11-17 15:55:15 -0800

    Milestone 1 complete

---
+++ Assets/Scripts/JumpPadController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPadController : MonoBehaviour
+{
+    public int speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+             rb.AddForce(Vector3.up * speed);
+        }
+    }
+}
